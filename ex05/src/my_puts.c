b5ecf0a6e67d12be64f0dea18acb88caa7b9c25d
Alexander Kolesnik <alex60070@gmail.com>
2018-06-11 17:15:18 +0300

    4-itoa done

---
+++ ex05/src/my_puts.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <string.h>
+int my_puts(const char *s){
+  write(1, s , strlen(s)*sizeof(char));
+  write(1, "\n" , 1 );
+  return 0;
+}
+//  const char msg[] = "Test msg;
