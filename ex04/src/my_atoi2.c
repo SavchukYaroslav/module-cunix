990a68f59005cb9bf8e264d121b3ad4170e6996e
alexander <alex60070@gmail.com>
2018-06-13 22:02:53 +0300

    ex04:made look pretty

--- ex04/src/my_atoi2.c
+++ ex04/src/my_atoi2.c
@@ -6,35 +6,16 @@ int my_atoi(const char *nptr){
   int res = 0;
   int mult = 1;
   char c;
-
   int i = n-1;
-  int x = ('0'<=nptr[i]) && (nptr[i] <= '9');
-  printf("Bool val: %d",x);
-  while ((i>=0) && !(('0'<=nptr[i])&&(nptr[i]<='9'))){
+
+  while ((i>=0) && (('0'>nptr[i])||(nptr[i]>'9')))
     i--;
-  }
-
-  printf("Symbol:%c\n", nptr[i]);
-   for(; i>=0 ; i--){
-     printf("Loop: %c\n", nptr[i]);
-     c = nptr[i];
-     if(c == '-')
+  for(; i>=0 ; i--){
+    c = nptr[i];
+    if((c == '-') || !(('0' <= c)&&( c <= '9')))
       break;
-
-     if (!(('0' <= c)&&( c <= '9')))
-      break;   
-
     res = res + mult*(c - '0' );
     mult = mult*10;
   }
-   if(c == '-')
-     return -res;
-   return res;
-
+  return c == '-'?-res:res;
 }
-/*
-int main(){
-  printf("%d", my_atoi("123456789"));
-} 
-*/
-
