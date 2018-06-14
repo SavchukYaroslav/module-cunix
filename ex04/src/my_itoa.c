180fa5fc15a789004962a19dec2ef88e0e0a5240
Alexander Kolesnik <alex60070@gmail.com>
2018-06-14 10:51:13 +0300

    ex04:my_itoa refactored

--- ex04/src/my_itoa.c
+++ ex04/src/my_itoa.c
@@ -9,49 +9,32 @@ long ten_pow(int n){
 }
 
 char* my_itoa(int nmbr){
-char* zero = malloc(sizeof(char));
-*zero = '0';
-  if(nmbr == 0)
+  if(nmbr == 0){
+    char* zero = malloc(sizeof(char));*zero = '0';
     return zero; 
+  }
   char* str = calloc(12, sizeof(char));
   str[11] = '\0';
   int sign = 1;
+
   if(nmbr < 0){
     sign = -1;
     nmbr = nmbr*(-1);
   }
-  
-  long a = nmbr;
   int i = 0;
-  while(a != 0){
-    printf("Inside while, a: %d\n", a);
-    printf("I : %d\n", i);
+  for(long a = nmbr; a!=0 ;i++){
     int k = ten_pow(i+1);
     int r = a % k;
     a = a-r;
     r = r/(k/10);
-    printf("R:%d\n", r);
     str[10-i] = '0'+r;
-    i++;
   }
   if(i == 10)
     str[1] = '1';
-  //sign = -1;
-  if(sign == -1){
+  if(sign == -1)
     str[10-i] = '-';
-  }
   else
     i--;
-
   char* res = &str[10-i];
- // free(str);
   return res;
 }
-
-/*
-int main(){
-  printf("STR: %s", my_itoa(0));
-}
-*/
-
-
