f105bacc71c25a46d8b3e3d35973aa290691bc12
Alexander Kolesnik <alex60070@gmail.com>
2018-06-22 12:51:54 +0300

    printf done

--- ex11/src/echo.c
+++ ex11/src/echo.c
@@ -6,36 +6,6 @@
 #include "../include/hash.h"
 
 extern hashtable_t *ht;
-// static int ls(int c, char *v[]){
-    
-// }
-
-//         struct dirent *pDirent;
-//         DIR *pDir;
-
-//         if (c < 2) {
-//             printf ("$_>Usage: ls <dirname>\n");
-//             return 1;
-//         }
-
-//         pDir = opendir (v[1]);
-//         if (pDir == NULL) {
-//             printf ("$_>Cannot open directory '%s'\n", v[1]);
-//             return 1;
-//         }
-
-//         printf("$_>");
-//         while ((pDirent = readdir(pDir)) != NULL) {
-//             if(strcmp(pDirent->d_name, ".") && strcmp(pDirent->d_name, ".."))
-//                 printf ("%s ", pDirent->d_name);
-//         }
-//         printf("\n");
-//         closedir (pDir);
-//         return 1;
-// }
-static int ls(int c, char *v[]){
-    printf("$_> Echo worked\n");
-}
 
 int echo_validate(int argc, char** args){
     return 1;
@@ -71,7 +41,7 @@ int echo_execute(int argc, char** args){
             return 1;
         }
         else{
-            printf("%s \n", evaluated);
+            printf("%s ", evaluated);
             free(evaluated);
         }
     }
