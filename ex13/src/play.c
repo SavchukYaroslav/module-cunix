1ccb0d6de2bb545e43d7e24229a48ccd09915744
Alexander Kolesnik <alex60070@gmail.com>
2018-06-28 13:02:18 +0300

    diagonal alg created

--- ex13/src/play.c
+++ ex13/src/play.c
@@ -6,6 +6,10 @@
 #include "../include/filler.h"
 #include "../include/stream.h"
 
+
+static int tactics = 1;
+static int dir = 0;
+
 int cell_inside_map(int x, int y, filler_t *filler, info_t *info){
     return (x < filler->w) && (x >= 0) && (y < filler->h) && (y >= 0);
 }
@@ -31,17 +35,6 @@ int cell_nghbr_exist(int x, int y, filler_t *filler, info_t *info){
 }
 
 int can_put_figure(int x, int y, filler_t *filler, info_t *info){
-    // for(int i = 0; i < info->fig_h; i++){
-    //     for(int j = 0; j < info->fig_w; j++){
-    //       if  ((info->figure[i][j] == '*')
-    //         && cell_inside_map(x+j, y+i, filler, info)
-    //         && cell_is_empty(x+j, y+i, filler, info)
-    //         && cell_nghbr_exist(x+j, y+i, filler, info)){
-    //             return 1;
-    //         }
-
-    //     }
-    // }
       int neigh_found;
       
       neigh_found = 0;
@@ -61,6 +54,81 @@ int can_put_figure(int x, int y, filler_t *filler, info_t *info){
       return neigh_found;
   }
 
+
+pos_t silly_direction(filler_t *filler, info_t *info){
+  pos_t res;
+  int w = filler->w;
+  int h = filler->h;
+
+  if(dir == 1){
+    for(int i = 0; i < h; i++){
+      for(int j = w-1; j >= 0; j--){
+         if(can_put_figure(j, i, filler, info)){
+             res.x = j;
+             res.y = i;
+             return res;
+         } 
+      } 
+   } 
+   res.x = -1;
+   res.y = -1;
+   return res;
+  }
+
+  if(dir == 0){
+    for(int j = 0; j < w; j++){
+      for(int i = 0; i < h; i++){
+         if(can_put_figure(j, i, filler, info)){
+             res.x = j;
+             res.y = i;
+             return res;
+         } 
+      } 
+   } 
+   res.x = -1;
+   res.y = -1;
+   return res;
+  }
+
+  if(1){
+    for(int i = h-1; i >= 0; i--){
+      for(int j = w-1; j >= 0; j--){
+         if(can_put_figure(j, i, filler, info)){
+             res.x = j;
+             res.y = i;
+             return res;
+         } 
+      } 
+   } 
+   res.x = -1;
+   res.y = -1;
+   return res;
+  }
+
+}
+
+pos_t silly(filler_t *filler, info_t *info){
+  pos_t res;
+  int w = filler->w;
+  int h = filler->h;
+
+  for(int i = h-1; i >= 0; i--){
+    for(int j = 0; j < w; j++){
+  // for(int i = 0; i < h; i++){
+  //   for(int j = 0; j < w; j++){
+        if(can_put_figure(j, i, filler, info)){
+            res.x = j;
+            res.y = i;
+            return res;
+        }
+     }
+  }
+  res.x = -1;
+  res.y = -1;
+  return res;
+  
+}
+
 pos_t simple_diagonal(filler_t *filler, info_t *info){
   pos_t res;
   int w = filler->w;
@@ -79,42 +147,53 @@ pos_t simple_diagonal(filler_t *filler, info_t *info){
   }
 }
 
+int elem_is_wall(filler_t *filler, info_t *info, int x, int y){
+ if((x == 0) || (y == 0) ||
+     (x == filler->w - 1) || (y == filler->h - 1))
+      return 1;
+  else
+      return 0; 
+}
 
-pos_t silly(filler_t *filler, info_t *info){
-  pos_t res;
-  int w = filler->w;
-  int h = filler->h;
-  char** brd = filler->board;
-  char s = filler->symbol;
-
-
-  for(int i = h-1; i >= 0; i--){
-    for(int j = 0; j < w; j++){
-  // for(int i = 0; i < h; i++){
-  //   for(int j = 0; j < w; j++){
-        if(can_put_figure(j, i, filler, info)){
-            res.x = j;
-            res.y = i;
-            return res;
-        }
+int wall_reached(filler_t *filler, info_t *info, pos_t pos){
+  for(int i = 0; i < info->fig_h; i++){
+    for(int j = 0; j < info->fig_w; j++){
+      if(info->figure[i][j] == '*'){
+        if(elem_is_wall(filler, info, pos.x+j, pos.y+i))
+          return 1;
+       }
      }
   }
-  res.x = -1;
-  res.y = -1;
-  return res;
-  
+  return 0;
+    return elem_is_wall(filler, info, pos.x, pos.y);
 }
+
+pos_t make_diagonals(filler_t *filler, info_t *info){
+    pos_t res;
+
+    if(dir == 5)
+        dir = 0;
+
+    res = silly_direction(filler, info);
+    
+    if(res.x < 0) // no matches found
+      return res;
+    
+    if(wall_reached(filler, info, res))
+        dir++;
+   
+    return res;
+}
+
 pos_t         play(filler_t *filler, info_t *info){
-  pos_t res;
-  int w = filler->w;
-  int h = filler->h;
-  char** brd = filler->board;
-  char s = filler->symbol;
-  static int tactics = 0;
 
   if(tactics == -1)
     return simple_diagonal(filler, info);
-  if(tactics == 0){
+  if(tactics == 0)
     return silly(filler, info);
-  }
+  if(tactics == 1)
+    return make_diagonals(filler, info);
+  if(tactics == 3)
+    return silly_direction(filler, info);
+    
 }
